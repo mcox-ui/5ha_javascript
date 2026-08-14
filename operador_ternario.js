4eb5687b120d2a239cca344ab44d5fc02ffd42ba
Mauricio Cox <mcox@stbrendans.edu.ar>
2026-08-14 15:31:23 -0300

    Operadores logicos + ejercicios + Puedo jubilarme

---
+++ operador_ternario.js
@@ -0,0 +1,12 @@
+// Sintaxis De Los Operadores Ternarios
+
+// let numeroDeTragos = 1;
+
+// numeroDeTragos > 0 ? alert("Usted no puede manejar.") : alert("Nos alegra que sea un conductor responsable.")
+
+
+let nombre = prompt("¿Cuál es tu nombre?.");
+
+let response = nombre ? "Respuesta recibida." : "No recibimos tu respuesta. Por favor, decime cuál es tu nombre."
+
+alert(response);
